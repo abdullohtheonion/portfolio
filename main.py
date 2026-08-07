37ae3a2b5ea0b0409cf87e46df928d17a41427ee
Abdulloh <generalmrabdulloh@gmail.com>
2026-08-07 08:31:18 +0500

    Render app titles in 2 columns (day22)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -16,3 +17,15 @@ with col2:
     st.info(content)
 
 st.text("This is the end! Hold your breath, and count to ten! Feel the earth move, and then. Hear my heart burst agaaiin. For this is the end! I've drowned and dreamt this moment.")
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
