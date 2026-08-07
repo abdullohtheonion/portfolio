4bce67b538f7badae5bc4bf55285b89666b95601
Abdulloh <generalmrabdulloh@gmail.com>
2026-08-07 10:38:16 +0500

    Add app description, image, and links.(day22)

--- main.py
+++ main.py
@@ -18,14 +18,20 @@ with col2:
 
 st.text("This is the end! Hold your breath, and count to ten! Feel the earth move, and then. Hear my heart burst agaaiin. For this is the end! I've drowned and dreamt this moment.")
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, .5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write("[Source code](https://youtu.be/dQw4w9WgXcQ?si=i8oH61ZEi7IvvEpk)")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write("[Source code](https://youtu.be/dQw4w9WgXcQ?si=i8oH61ZEi7IvvEpk)")
